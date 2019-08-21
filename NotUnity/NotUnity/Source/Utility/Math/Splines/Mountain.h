63f55d24e8b28fbe5e48f902064e98b6cb407d8a
InTheAxis <codeaxex@gmail.com>
2019-08-21 16:59:07 +0800

    changed spline, added mountain spline

---
+++ NotUnity/NotUnity/Source/Utility/Math/Splines/Mountain.h
@@ -0,0 +1,22 @@
+#ifndef SPLINE_MT_H
+#define SPLINE_MT_H
+
+#include <vector>
+
+#include "../Vector3.h"
+#include "../Spline.h"
+
+class SplineMountain : public Spline
+{
+public:
+	SplineMountain();
+	~SplineMountain();
+
+	virtual float Fn(float x);
+	virtual Vector3 Normal(float x);
+
+private:
+	float offset;
+};
+
+#endif
